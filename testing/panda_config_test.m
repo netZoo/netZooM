dac58b0efeccedc31550a3ae01f3cff5b71939dc
marouenbg <marouen.b.guebila@gmail.com>
2019-05-28 11:12:43 -0400

    added test files

---
+++ testing/panda_config_test.m
@@ -0,0 +1,8 @@
+% Set Program Parameters
+exp_file   = 'test_data/expression.txt';
+motif_file = 'test_data/motif.txt';
+ppi_file   = 'test_data/ppi.txt';
+panda_out  = '/tmp/panda.test.txt';  % optional, leave empty if file output is not required
+save_temp  = '/tmp';  % optional, leave empty if temp data files will not needed afterward
+lib_path   = 'lib';  % path to the folder of PANDA source code
+alpha      = 0.1;
