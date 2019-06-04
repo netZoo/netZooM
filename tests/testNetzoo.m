8fee2ba434255cde0df147461165e2f725ef5093
marouenbg <marouen.b.guebila@gmail.com>
2019-06-04 16:42:31 -0400

    add test data network

--- tests/testNetzoo.m
+++ tests/testNetzoo.m
@@ -7,9 +7,9 @@ if isOctave
 end
 
 % Set Program Parameters
-exp_file   = 'test_data/expression.txt';
-motif_file = 'test_data/motif.txt';
-ppi_file   = 'test_data/ppi.txt';
+exp_file   = 'expression.txt';
+motif_file = 'motif.txt';
+ppi_file   = 'ppi.txt';
 panda_out  = 'tmp/panda.test.txt';  % optional, leave empty if file output is not required
 save_temp  = '';  % optional, leave empty if temp data files will not be needed afterward
 lib_path   = '../netzoo-m';  % path to the folder of PANDA source code
@@ -20,8 +20,13 @@ save_pairs = 0;%saving in .pairs format
 addpath(genpath(fullfile(pwd,'../netzoo-m')));
 
 % Create save folder
-pwd
 mkdir tmp;
 
 % Call Panda
-AgNet=panda_run(lib_path,exp_file, motif_file, ppi_file, panda_out, save_temp, alpha, save_pairs);
+AgNet = panda_run(lib_path,exp_file, motif_file, ppi_file, panda_out, save_temp, alpha, save_pairs);
+
+% Load the expected result
+ExpAgNet = textread('panda.test.txt');
+
+% Compare the outputs
+assert(isequal(AgNet,ExpAgNet));
