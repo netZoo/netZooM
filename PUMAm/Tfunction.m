462e0388500cfb9c65f5560bf8c1e0de7d9122f3
marouenbg <marouen.b.guebila@gmail.com>
2019-07-10 13:08:05 -0400

    deleted pumac files

--- PUMAm/Tfunction.m
+++
@@ -1,7 +0,0 @@
-function Amat=Tfunction(X,Y);
-
-Amat=(X*Y);
-Bmat=repmat(sum(Y.^2,1), size(X,1), 1);
-Cmat=repmat(sum(X.^2,2), 1, size(Y,2));
-
-Amat=Amat./sqrt(Bmat+Cmat-abs(Amat));
--- PUMAm/UpdateDiagonal.m
+++
@@ -1,5 +0,0 @@
-function diagMat=UpdateDiagonal(diagMat, num, alpha, step);
-
-diagMat(1:(num+1):end)=nan;
-diagstd=nanstd(diagMat,1);
-diagMat(1:(num+1):end)=diagstd*num*exp(2*alpha*step);
