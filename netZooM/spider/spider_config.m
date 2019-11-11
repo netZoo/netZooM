d601decc671870a80b9b8590424d8d14d92ad0b4
Abhijeet  Sonawane <spaso@nantucket.bwh.harvard.edu>
2019-11-11 17:41:57 -0500

    spider tests data added

--- netZooM/spider/spider_config.m
+++ netZooM/spider/spider_config.m
@@ -1,10 +1,10 @@
 % Set Program Parameters
 motifhitfile = '/SPIDER_code/InputData/EpiMotifFiles/GM12878_filtered_motiflocations.bed'; % file storing epigenetically informed motif information, can be created with CreateEpigeneticMotif.m
-regfile     = '/SPIDER_code/InputData/RegulatoryRegions/ProximalRegulatoryRegions.bed'; % file containing regulatory regions for genes, can be created with DefineRegulatoryRegions.m
+regfile     = '/SPIDER_code/InputData/RegulatoryRegions/RegulatoryRegions_0-1kb.bed'; % file containing regulatory regions for genes, can be created with DefineRegulatoryRegions.m
 
 annofile    = 'InputData/ReferenceData/refseq_hg19_05292018'; % file with gene annotations
 chrinfo     = 'InputData/ReferenceData/GenomeWideRanges.bed'; % file with chromosome information
-ranges      = {[-1000,+500]};
+ranges      = {[-1000,+1000]};
 
 motifdir    = '/SPIDER_code/InputData/MotifBedFiles/'; % where the original motif scan files are stored (one bed file per motif)
 epifile     = '/SPIDER_code/InputData/DNaseBedFiles/GM12878_DnasePeaks.bed'; % file with open chromatin regions
