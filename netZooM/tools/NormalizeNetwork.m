a04d88c15241f3d3666b3975baca1b6c73114dc1
marouenbg <marouen.b.guebila@gmail.com>
2020-01-30 21:27:30 -0500

    reduce memory of normalize network

--- netZooM/tools/NormalizeNetwork.m
+++ netZooM/tools/NormalizeNetwork.m
@@ -20,12 +20,13 @@ function normMat = NormalizeNetwork(X)
 
     Z1=(X-repmat(mu1, size(X,1), 1))./repmat(std1, size(X,1), 1);
     Z2=(X-repmat(mu2, 1, size(X,2)))./repmat(std2, 1, size(X,2));
-    normMat=Z1/sqrt(2)+Z2/sqrt(2);
+    
 
     % checks and defaults for missing data
-    Z0=(X-mu0)/std0;
+    Z0=(X-mu0)/std0;clear X;
+    normMat=Z1/sqrt(2)+Z2/sqrt(2);
     f1=isnan(Z1); f2=isnan(Z2);
-    normMat(f1)=Z2(f1)/sqrt(2)+Z0(f1)/sqrt(2);
-    normMat(f2)=Z1(f2)/sqrt(2)+Z0(f2)/sqrt(2);
+    normMat(f1)=Z2(f1)/sqrt(2)+Z0(f1)/sqrt(2);clear Z2;
+    normMat(f2)=Z1(f2)/sqrt(2)+Z0(f2)/sqrt(2);clear Z1;
     normMat(f1 & f2)=2*Z0(f1 & f2)/sqrt(2);
 end
