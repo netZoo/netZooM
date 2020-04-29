d6dc9d00b237707aa935c16af7c523461ee95c59
marouenbg <marouen.b.guebila@gmail.com>
2020-04-28 22:19:10 -0400

    added test

--- netZooM/otter/otter.m
+++ netZooM/otter/otter.m
@@ -1,9 +1,40 @@
-function W = otter(W,P,C)
-%parameters
-lambda = 0.0035;
-gamma = 0.335;
-Imax = 300;
-eta = 0.00001;
+function W = otter(W,P,C,lambda,gamma,Imax,eta)
+% Description:
+%               OTTER infers gene regulatory networks using TF DNA binding
+%               motif (W), TF PPI (P), and gene coexpression (C) through 
+%               minimzing the following objective:
+%                                  min f(W) 
+%               with f(W) = (1-lambda)/4*||WW' - P||^2 + (lambda/4)*||W'W - C||^2 + (gamma/2)*||W||^2
+%
+% Inputs:
+%               W     : TF-gene regulatory network based on TF motifs as a
+%                       matrix of size (t,g), g=number of genes, t=number of TFs
+%               P     : TF-TF protein interaction network as a matrix of size (t,t)
+%               C     : gene coexpression as a matrix of size (g,g) 
+%               lambda: 
+%               gamma : penalization term
+%               Imax  : number of iterations of the algorithm
+%               eta   : 
+%                       
+% Outputs:
+%               W  : Predicted TF-gene complete regulatory network as an adjacency matrix of size (t,g).
+%
+% Authors: 
+%               Rebekka Burkholz 4/2020
+
+%global parameters
+if nargin<4
+    lambda = 0.0035;
+end
+if nargin<5
+    gamma = 0.335;
+end
+if nargin<6
+    Imax = 300;
+end
+if nargin<7
+    eta = 0.00001;
+end
 %ADAM parameters
 b1 = 0.9;
 b2 = 0.999;
@@ -23,16 +54,16 @@ b2t = b2;
 P = -P*(1-lambda) + gamma*eye(nTF);
 C = -C*lambda;
 %ADAM gradient descent                  
-    for i = 1:Imax
-       %gradient
-       grad = (P*W + W*C + W*W'*W);
-       m = b1*m + ((1-b1)*4)*grad;
-       v = b2*v + ((1-b2)*16)*grad.^2; 
-       b1t = b1t*b1;
-       b2t = b2t*b2;
-       alpha = sqrt(1-b2t)/(1-b1t)*eta;
-       epst = eps*sqrt((1-b2t));
-       %update of gene ragulatory matrix
-       W = W - alpha*(m./(epst+sqrt(v)));
-    end
+for i = 1:Imax
+    %gradient
+    grad = (P*W + W*C + W*W'*W);
+    m = b1*m + ((1-b1)*4)*grad;
+    v = b2*v + ((1-b2)*16)*grad.^2; 
+    b1t = b1t*b1;
+    b2t = b2t*b2;
+    alpha = sqrt(1-b2t)/(1-b1t)*eta;
+    epst = eps*sqrt((1-b2t));
+    %update of gene ragulatory matrix
+    W = W - alpha*(m./(epst+sqrt(v)));
+end
 end
---
+++ tests/testOtter.m
@@ -0,0 +1,49 @@
+function test_suite=testOtter()
+	initTestSuite;
+end
+
+function testOtterSimple()
+	% Tell if this is Octave (Unit tests) or Matlab
+        isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+
+        % Load statistics package from Octave
+        if isOctave
+            %we need the nan package because it has a fast implementation of corrcoeff
+            %pkg load statistics
+            pkg load nan;
+            return % readtable is not available in octave
+        end
+
+        % Set Program Parameters
+        generatePriors=0;
+        if generatePriors==1
+            exp_file   = 'test_data/expression.txt';
+            motif_file = 'test_data/motifTest.txt';
+            ppi_file   = 'test_data/ppi.txt';
+            [Exp,RegNet,TFCoop,TFNames,GeneNames]=processData(exp_file,motif_file,ppi_file,'intersection');
+            tic; GeneCoReg = Coexpression(Exp); toc;
+            csvwrite('c.csv',GeneCoReg);
+            csvwrite('p.csv',TFCoop);
+            csvwrite('w.csv',RegNet);
+        elseif generatePriors==0
+            C=csvread('c.csv');
+            P=csvread('p.csv');
+            W=csvread('w.csv');
+        end
+
+        % Add path
+        addpath(genpath(fullfile(pwd,'tests')));
+
+        % Call Otter
+        tic;W = otter(W,P,C);toc;
+        
+        % Load the expected result
+        filename = 'test_data/otter/test_otter.csv';
+        W_test = csvread(filename);
+
+        % Compare the outputs
+        tolMat  =1e-6;
+        deltaMat=max(max(abs(W-W_test)));
+	    assertTrue( deltaMat < tolMat );
+        
+end
