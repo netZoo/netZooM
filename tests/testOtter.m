9d8cbcd4c4015efb3f1380e0076aa273a4c94734
marouenbg <marouen.b.guebila@gmail.com>
2020-04-28 22:30:30 -0400

    enabled test

--- tests/testOtter.m
+++ tests/testOtter.m
@@ -11,7 +11,6 @@ function testOtterSimple()
             %we need the nan package because it has a fast implementation of corrcoeff
             %pkg load statistics
             pkg load nan;
-            return % readtable is not available in octave
         end
 
         % Set Program Parameters
