7dcb0e9a8377c3dd72f1d25f7d4d7bd2dbcb023e
marouenbg <marouen.b.guebila@gmail.com>
2020-07-08 17:56:17 -0400

    pushed to GRAND

--- netZooM/tools/reproduceGrandNetwork.m
+++ netZooM/tools/reproduceGrandNetwork.m
@@ -1,110 +1,109 @@
-netZoo     = 'netZooM';
-netZooVer  = '0.5.1';
-netZooTool = 'OTTER';
-ppiLink    = 'https://granddb.s3.amazonaws.com/cancer/cervix_cancer/cancer_cervix_otter_ppi.csv';
-motifLink  = 'https://granddb.s3.amazonaws.com/cancer/cervix_cancer/cancer_cervix_otter_motif.csv';
-expLink    = 'https://granddb.s3.amazonaws.com/cancer/cervix_cancer/cancer_cervix_expression_tcga.csv';
-networkLink= 'https://granddb.s3.amazonaws.com/cancer/cervix_cancer/cancer_cervix_otter_network.csv';
+
 netRes=reproduceGrandNetworkFunction(ppiLink,motifLink,expLink,netZoo,netZooTool,...
     netZooVer,networkLink);
 
 function [netRes,repPres]=reproduceGrandNetworkFunction(ppiLink,motifLink,expLink,...
     netZoo,netZooTool,netZooVer,networkLink)
-% Description:
-%               reproduceGrandNetworkFunction fetches input data from GRAND (grand.networmedicine.org) 
-%               to recompute a regulatory network and compare it to the
-%               network from GRAND. Thsi function checks that GRAND
-%               networks are reproducible using netZoo (netzoo.github.io).
-%
-% Inputs:
-%               ppiLink    : url to file containing TF-TF interaction graph
-%               motifLink  : url to file containing the prior regulatory network
-%               expLink    : url to file containing gene expression as a
-%                            matrix of size (g,s) with s number of gene expression
-%                            samples and g the number of genes
-%               netZoo     : which implementation of netZoo e.g., 'netZooM'
-%               netZooTool : which zoo animal e.g., 'PUMA'
-%               netZooVer  : which netZoo version e.g., 0.5.1
-%               networkLink: url to file containing GRAND regulatory
-%                            network to comapre to
-% 
-% Outputs:
-%               netRes     : Predicted bipartite gene complete regulatory network 
-%                            using netZooTool as a matrix of size (t,g) with t the number of regulators.
-%               repPres    : largest absolute difference between recomputed network
-%                             and GRAND network
-%
-% Dependencies:
-%               AWSCLI    : https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
-%               GIT       : https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
-%
-% Authors: 
-%               Marouen Ben Guebila 6/2020
+%% Description:
+%%               reproduceGrandNetworkFunction fetches input data from GRAND (grand.networmedicine.org) 
+%%               to recompute a regulatory network and compare it to the
+%%               network from GRAND. Thsi function checks that GRAND
+%%               networks are reproducible using netZoo (netzoo.github.io).
+%%
+%% Inputs:
+%%               ppiLink    : url to file containing TF-TF interaction graph
+%%               motifLink  : url to file containing the prior regulatory network
+%%               expLink    : url to file containing gene expression as a
+%%                            matrix of size (g,s) with s number of gene expression
+%%                           samples and g the number of genes
+%%               netZoo     : which implementation of netZoo e.g., 'netZooM'
+%%               netZooTool : which zoo animal e.g., 'PUMA'
+%%               netZooVer  : which netZoo version e.g., 0.5.1
+%%               networkLink: url to file containing GRAND regulatory
+%%                            network to comapre to
+%% 
+%% Outputs:
+%%               netRes     : Predicted bipartite gene complete regulatory network 
+%%                            using netZooTool as a matrix of size (t,g) with t the number of regulators.
+%%               repPres    : largest absolute difference between recomputed network
+%%                             and GRAND network
+%%
+%% Dependencies:
+%%               AWSCLI    : https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
+%%               GIT       : https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+%%
+%% Authors: 
+%%               Marouen Ben Guebila 6/2020
 
-    % Parse args
+    %% Parse args
     if isequal(netZooTool,'PANDA-LIONESS')
         netZooTool = 'LIONESS';
     end
-    % Check dependencies
-    % 1. AWSCLI
+    %% Check dependencies
+    %% 1. AWSCLI
     try
        system('aws --version') 
     catch
         error('Please install awscli (https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) and try again.')
     end
-    % 2. GIT
+    %% 2. GIT
     try
        system('git --version') 
     catch
         error('Please install git (https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) and try again.')
     end
 
-    % Fetch netZoo
+    %% Fetch netZoo
     pathToZoo= fileparts([which([netZooTool '.m']) which([netZooTool '_run.m'])]);
     if isempty(pathToZoo)
         system(['git clone https://github.com/netZoo/' netZoo '.git']);
-        % Add to path
+        %% Add to path
         addpath(genpath(netZoo))
-        % Go to the root of netZoo
+        %% Go to the root of netZoo
         eval(['cd ' netZoo]);
     else
-        % Go to the root of netZoo
+        %% Go to the root of netZoo
         cd(pathToZoo);cd ../..
     end
-    % Switch to specific version
+    %% Switch to specific version
     system(['git checkout tags/' netZooVer]);
 
-    % Fetch priors
-    % change prefix to ssh
+    %% Fetch priors
+    %% change prefix to ssh
     motifLinkssh   =['s3://granddb/' motifLink(34:end)];
     expLinkssh     =['s3://granddb/' expLink(34:end)];
     ppiLinkssh     =['s3://granddb/' ppiLink(34:end)];
     networkLinkssh =['s3://granddb/' networkLink(34:end)];
-    % Motif
+    %% Motif
     system(['aws s3 cp ' motifLinkssh ' .']);
-    % Exp
+    %% Exp
     system(['aws s3 cp ' expLinkssh ' .']);
-    % PPI
+    %% PPI
     if ~isequal(ppiLinkssh,'-')
         system(['aws s3 cp ' ppiLinkssh ' .'])
+    else
+        
     end
-    % GRAND network for comparison
+    %% GRAND network for comparison
     system(['aws s3 cp ' networkLinkssh ' .'])
 
-    % Run tool
-    % parse file name
-    % Network
+    %% Run tool
+    %% parse file name
+    %% Network
     [~,fileNet,extNet]=fileparts(networkLink);
     net_file  =[fileNet extNet];
-    % Expression
+    %% Expression
     [~,fileExp,extExp]=fileparts(expLink);
     exp_file  =[fileExp extExp];
-    % Motif
+    %% Motif
     [~,fileMotif,extMotif]=fileparts(motifLink);
     motif_file=[fileMotif extMotif];
-    % PPI
+    %% PPI
     [~,filePPI,extPPI]=fileparts(ppiLink);
     ppi_file  =[filePPI extPPI];
+    if isequal(ppi_file,'-')
+       ppi_file=''; 
+    end
     lib_path='';
     switch netZooTool
         case 'PANDA'
@@ -128,9 +127,13 @@ function [netRes,repPres]=reproduceGrandNetworkFunction(ppiLink,motifLink,expLin
             netRes=otter(Motif{:,:},PPI{:,:},C);
     end
 
-    % Compare to result network
-    netGrand  = readtable(net_file,'ReadVariableNames',1,'ReadRowNames',1);
-    % compute precision difference to check reproducibility
-    repPres = max(max( abs(netGrand{:,:} - netRes) ));
+    if isequal(netZooTool,'LIONESS')
+        %% Compare to result network
+        netGrand  = readtable(net_file,'ReadVariableNames',1,'ReadRowNames',1);
+        %% compute precision difference to check reproducibility
+        repPres = max(max( abs(netGrand{:,:} - netRes) ));
+    else
+        repPres = [];
+    end
 
 end
