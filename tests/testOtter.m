12e50869de174f80e1b26632a55f8d4d848a198a
marouenbg <marouen.b.guebila@gmail.com>
2022-07-07 18:59:42 -0400

    update tests

--- tests/testOtter.m
+++ tests/testOtter.m
@@ -1,5 +1,9 @@
 function test_suite=testOtter()
-	initTestSuite;
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
 end
 
 function testOtterSimple()
