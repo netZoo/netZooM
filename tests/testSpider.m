ccde9f0e0d1cc02f9b8ee407b50176006581b880
marouenbg <marouen.b.guebila@gmail.com>
2022-07-11 21:03:08 -0400

    update workflow

--- tests/testSpider.m
+++ tests/testSpider.m
@@ -27,7 +27,11 @@ function testSpiderSimple()
         
         motifdir     = 'tests/spider/motifs/'; % where the original motif scan files are stored (one bed file per motif)
         epifile      = 'tests/spider/A549_DnasePeaks.bed'; % file with open chromatin regions
-        bedtoolspath = '/home/runner/work/netZooM/netZooM/bedtools2/bin/'; 
+        if isunix
+            bedtoolspath = './bedtools2/bin/'; 
+        elseif ismac
+            bedtoolspath = './';
+        end
         outtag = 'tests/output/';
         
         spider_out  = 'tests/spider/output/A549_5TF_100Genes_casenet.txt';  % optional, leave empty if file output is not required
