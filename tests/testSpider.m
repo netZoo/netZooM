532e0f7b805d67cd4497951ec698af74c8b5de1c
marouenbg <marouen.b.guebila@gmail.com>
2022-07-14 21:09:48 -0400

    separte process functions

--- tests/testSpider.m
+++ tests/testSpider.m
@@ -27,12 +27,7 @@ function testSpiderSimple()
         
         motifdir     = 'tests/spider/motifs/'; % where the original motif scan files are stored (one bed file per motif)
         epifile      = 'tests/spider/A549_DnasePeaks.bed'; % file with open chromatin regions
-
-        if ismac
-            bedtoolspath = './';
-        elseif isunix
-            bedtoolspath = './bedtools2/bin/'; 
-        end
+        bedtoolspath = './bedtools2/bin/'; 
         outtag = 'tests/output/';
         
         spider_out  = 'tests/spider/output/A549_5TF_100Genes_casenet.txt';  % optional, leave empty if file output is not required
